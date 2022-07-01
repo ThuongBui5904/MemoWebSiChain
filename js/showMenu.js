a119ae1f59e2cdc97afb1e217fdb93230e818219
ThuongBuiOds <govarava@gmail.com>
2022-07-01 21:35:52 +0700

    mobile reponsive

---
+++ js/showMenu.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    var windowWidth = $(window).width();
+    //alert(windowWidth);
+    if(windowWidth<860)
+	{
+        $(".categories ul").hide();
+    }
+    //$(".categories ul").hide();
+    $(".menu-icon").click(function() {
+        $(".categories ul").toggle();
+    });
+    $(".categories ul li").click(function(){
+        $(".categories ul").hide();
+    })
+});
